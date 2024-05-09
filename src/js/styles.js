137729d950dc1a5f8f608130ee971b40f206da00
jordon266 <jordon.j.murray@torontomu.ca>
2024-05-09 19:07:56 -0400

    added_home

--- src/js/styles.js
+++ src/js/styles.js
@@ -1,10 +1,19 @@
+
+
 // selectors
 
-const todoInputs = document.querySelector(".todo-inputs");
+const todoInputs = document.querySelector(".gpa_name-inputs");
+const gpa_desc = document.querySelector(".gpa_desc-inputs");
+const actual_gpa = document.querySelector(".actual_gpa-inputs");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector(".filter-todos");
-
+const list_of_GPAMarkers = new Array();
+let display_gpa_name = document.querySelector(".display_gpa_name");
+let display_gpa_desc = document.querySelector(".display_gpa_desc ");
+let display_actual_gpa = document.querySelector(".display_gpa");
+const gpa_name_item = document.querySelector(".gpa_name_item")
+let number = 0;
 //event listeners
 document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodos);
@@ -12,22 +21,26 @@ todoList.addEventListener("click", deleteCheck);
 //filterOption.addEventListener("click", filterTodo);
 
 function addTodos(e) {
+  
   e.preventDefault();
-
+  g = {gpa_name: todoInputs.value,gpa_desc: gpa_desc.value,actual_gpa:actual_gpa.value}
+  list_of_GPAMarkers.push(g);
+  console.log(list_of_GPAMarkers)
   //tododiv
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
 
   //new todo
   const newTodo = document.createElement("li");
-  newTodo.classList.add("todo-item");
+  newTodo.classList.add("gpa_name_item");
   newTodo.innerText = todoInputs.value;
   todoDiv.appendChild(newTodo);
 
   //checked button
   const compleatedButton = document.createElement("button");
   compleatedButton.innerHTML = '<i class="fas fa-check"><i>';
-  compleatedButton.classList.add("complete-btn");
+  compleatedButton.classList.add("complete-btn" + number);
+  number+=1
   todoDiv.appendChild(compleatedButton);
   //trash button
   const trashButton = document.createElement("button");
@@ -44,7 +57,7 @@ function deleteCheck(e) {
 
   //delete todo
   if (items.classList[0] === "trash-btn") {
-    const todo = items.parentElement;
+    const todo = items.parentElement.li;
 
     //todo animation
     todo.classList.add("fall");
@@ -54,9 +67,15 @@ function deleteCheck(e) {
   }
 
   //completed btn
-  if (items.classList[0] === "complete-btn") {
+  if (items.classList[0].startsWith("complete-btn")) {
+    
     const todo = items.parentElement;
-    todo.classList.toggle("completed");
+    g = list_of_GPAMarkers[parseInt((items.classList[0]).match(/(\d+)/)[0])]
+ display_gpa_marker(g)
+  todo.classList.toggle("completed");
+    
+  
+
   }
 }
 
@@ -112,4 +131,58 @@ function getTodos() {
     //append list
     todoList.appendChild(todoDiv);
   });
+}
+
+
+// Home html
+const select = document.getElementById("basic-select");
+const menu = document.getElementById("option-select");
+const dropdowns = document.querySelectorAll(".dropdown");
+const cart = document.querySelector(".carpet");
+const options = document.querySelectorAll(".options .options__item");
+const selected = document.getElementById("select-title");
+var active = document.querySelector(".options__item--active");
+const animTime = 120;
+
+select.addEventListener('mouseenter', function(){
+  select.classList.add("selected--open");
+  cart.classList.add("carpet--open");
+  setTimeout(function(){
+    select.classList.add("selected--delay");
+  }, animTime);
+})
+select.addEventListener('mouseleave', function(){
+  select.classList.remove("selected--open");
+  cart.classList.remove("carpet--open");
+  setTimeout(function(){
+    select.classList.remove("selected--delay");
+  }, animTime);
+})
+menu.addEventListener('mouseenter', function(){
+  select.classList.add("selected--open");
+  cart.classList.add("carpet--open");
+  setTimeout(function(){
+    select.classList.add("selected--delay");
+  }, animTime);
+})
+menu.addEventListener('mouseleave', function(){
+  select.classList.remove("selected--open");
+  cart.classList.remove("carpet--open");
+  setTimeout(function(){
+    select.classList.remove("selected--delay");
+  }, animTime);
+})
+options.forEach(option => {
+  option.addEventListener("click", function(){
+    selected.innerText = option.innerText;
+    active.classList.remove("options__item--active");
+    option.classList.add("options__item--active");
+    active = document.querySelector(".options__item--active");
+  });
+});
+
+function display_gpa_marker(GPAMarker){
+display_gpa_name.innerText = GPAMarker.gpa_name;
+display_gpa_desc.innerText = GPAMarker.gpa_desc;
+display_actual_gpa.innerText = GPAMarker.actual_gpa
 }
