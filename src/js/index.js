d4363a1c28e6ed62bd1b7e5f6a50e1899991c96c
Ewcw17 <126487460+Ewcw17@users.noreply.github.com>
2024-05-30 16:49:42 -0400

    Made my pages work with express

--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,20 @@ console.log("here is the path ->"+__dirname)
 app.use(express.static(path.join(__dirname,'../')));
 // console.log("new path hopefully " + __dirname);
 let htmlPath = path.join(__dirname,'../html/')
+// .. notation does work on windows
+console.log("new path hopefully " + htmlPath);
 // app.use(express.static(htmlPath));
 app.get('/', (req,res)=>{
     res.sendFile(`${htmlPath}home.html`);
 });
 app.get('/yeargpa', (req,res)=>{
     res.sendFile( `${htmlPath}cumulgpa.html`);
+});
+app.get('/coursegpa', (req,res)=>{
+    res.sendFile( `${htmlPath}add_assessment.html`);
+});
+app.get('/semestergpa', (req,res)=>{
+    res.sendFile( `${htmlPath}add_course.html`);
 });
 
 app.listen(4000);
